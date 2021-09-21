62cc1ea00d3f59cd7098eb0b2d39612548d931f3
Overload86 <overload86@gmx.de>
2021-09-22 01:03:58 +0200

    Clang-Formatted all files

--- Hazel/src/Hazel/Layer.cpp
+++ Hazel/src/Hazel/Layer.cpp
@@ -1,12 +1,8 @@
 #include "hzpch.h"
+
 #include "Layer.h"
 
 namespace Hazel {
-	Layer::Layer(const std::string& debugname)
-		: m_DebugName(debugname)
-	{
-	}
-	Layer::~Layer()
-	{
-	}
-}
+Layer::Layer(const std::string& debugname) : m_DebugName(debugname) {}
+Layer::~Layer() {}
+} // namespace Hazel
